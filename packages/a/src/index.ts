befa9c85048505111f0ff9984b9e3d81d3ce93f0
谭新 <tanxin.byte@bytedance.com>
2023-01-19 17:03:15 +0800

    重新初始化

--- packages/a/src/index.ts
+++ packages/a/src/index.ts
@@ -1,3 +1,6 @@
+import { print as dPrint } from '@changesets-demo/d'
+
 export function print() {
+  dPrint()
   console.log('module a')
 }
--- packages/c/src/index.ts
+++ packages/c/src/index.ts
@@ -1,3 +1,8 @@
+import { print as aPrint } from '@changesets-demo/a'
+import { print as bPrint } from '@changesets-demo/b'
+
 export function print() {
+  aPrint()
+  bPrint()
   console.log('module c')
 }
