28167686ede23d9591a153d3d0c8186c95945c14
Xin Tan <xin.tan@zoom.us>
2024-08-14 00:00:26 +0800

    feat(a): add feature for package a

--- packages/a/src/index.ts
+++ packages/a/src/index.ts
@@ -1,6 +1,6 @@
-import { print as dPrint } from '@greenhand/changeset-demo-d'
+import { print as dPrint } from "@greenhand/changeset-demo-d";
 
 export function print() {
-  dPrint()
-  console.log('module a')
+  dPrint();
+  console.log("module a", "xx");
 }
